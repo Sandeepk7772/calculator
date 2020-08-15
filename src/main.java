6555bbf17ddf22d8f1c9267e23a34d0ca2f2683c
Sandeep <sandeepk7772@gmail.com>
2020-08-15 00:09:32 -0700

    Initial commit
    - added functionality to get a sum of two numbers

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int result = getSum(2, 3);
+        System.out.println("Result is " + result);
+    }
+
+    public static int getSum (int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+}
