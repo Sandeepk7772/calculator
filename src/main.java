dc439212d77721cb40b7ad12b62612358cf484b0
Sandeep <sandeepk7772@gmail.com>
2020-08-15 00:43:47 -0700

    Fixed getSum and added getProduct and getSub

--- src/main.java
+++ src/main.java
@@ -2,9 +2,20 @@ public class main {
     public static void main(String[] args) {
         int result = getSum(2, 3);
         System.out.println("Result is " + result);
+        int result1=getmulti(6,8);
+        int result2=getsub(8,7);
     }
 
-    public static int getSum (int firstNumber, int secondNumber) {
-        return firstNumber - secondNumber;
+    public static int getSum (int firstNumber, int secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+    public static int getproduct (int newNumber1, int newNumber2)
+    {
+        return firstNumber * secondNumber;
+    }
+    public static int getsub (int firstNumber2, int secondNumber3)
+    {
+        return firstNumber2 - secondNumber3;
     }
 }
