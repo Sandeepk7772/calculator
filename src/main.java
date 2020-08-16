d68d153c0162aeb782ad730c173ad4c970e63aee
Sandeep <sandeepk7772@gmail.com>
2020-08-16 12:45:52 -0700

    added new operator lease enter the commit message for your changes. Lines starting

--- src/main.java
+++ src/main.java
@@ -1,21 +1,35 @@
+
+import java.util.*;
+
 public class main {
     public static void main(String[] args) {
-        int result = getSum(2, 3);
-        System.out.println("Result is " + result);
-        int result1=getmulti(6,8);
-        int result2=getsub(8,7);
+        double result=0.0;
+
+        Scanner object= new Scanner(System.in);
+
+        System.out .println("Enter the First Number:- ");
+        int num1=object.nextInt();
+        System.out.println("Enter the Second Number:- ");
+        int num2=object.nextInt();
+        System.out.println("What Operation? ");
+        char operator=object.next().charAt(0);
+
+        switch(operator){
+
+            case '+' : result = num1 + num2;
+            break;
+            case '-' : result = num1 - num2;
+            break;
+            case '*' : result = num1 * num2;
+            break;
+            case '/' : result = num1 / num2;
+            break;
+            default:
+                System.out.println("The operator does't match");
+
+        }
+        System.out.println(num1+" "+operator+" "+num2+" = "+result);
     }
 
-    public static int getSum (int firstNumber, int secondNumber)
-    {
-        return firstNumber + secondNumber;
-    }
-    public static int getproduct (int newNumber1, int newNumber2)
-    {
-        return firstNumber * secondNumber;
-    }
-    public static int getsub (int firstNumber2, int secondNumber3)
-    {
-        return firstNumber2 - secondNumber3;
-    }
+
 }
